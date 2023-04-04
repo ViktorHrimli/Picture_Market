1c266256775eb9718e45abcd2bdd73330b4414fb
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-04-04 23:35:42 +0300

    card

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import Swipeable from 'react-swipeable';
-import { HeaderConteiner, Section, HeaderLogo, HeaderBtn, HeaderBtnLin, ListLayout, LinkLayout, LinkBorder } from 'components/Header/Header.styled';
+import { Swipeable } from 'react-swipeable';
+import {
+  HeaderConteiner,
+  Section,
+  HeaderLogo,
+  HeaderBtn,
+  HeaderBtnLin,
+  ListLayout,
+  LinkLayout,
+  LinkBorder,
+} from 'components/Header/Header.styled';
 import { HeaderMenu } from 'components/HeaderMenu/HeaderMenu';
-  
+
 export function Header() {
   const [isClosedSection, setIsClosedSection] = useState(true);
   const [isOpenedNavigation, setIsOpenedNavigation] = useState(false);
@@ -21,34 +30,47 @@ export function Header() {
       setIsOpenedNavigation(false);
     }, 600);
 
-
-  const handleSwipe = (eventData) => {
-    if (eventData.dir === 'left') {
-      setIsClosedSection(true);
-    }
-  };
+    const handleSwipe = eventData => {
+      if (eventData.dir === 'left') {
+        setIsClosedSection(true);
+      }
+    };
 
     return () => clearTimeout(timeoutId);
   }, [isClosedSection]);
 
   return (
     <HeaderConteiner>
-    {isOpenedNavigation && (
-        <HeaderMenu isClosed={isClosedSection} setIsClosed={setIsClosedSection} />
-          )}
+      {isOpenedNavigation && (
+        <HeaderMenu
+          isClosed={isClosedSection}
+          setIsClosed={setIsClosedSection}
+        />
+      )}
       <Section>
-          <HeaderLogo src="https://res.cloudinary.com/djoprd9i4/image/upload/v1680452464/samples/picture_market/Group_128_4_zvenh9.png" width="100" height="75" alt="logo" />
-          <HeaderBtn onClick={handleClick}>
-            <HeaderBtnLin />
-            <HeaderBtnLin />
+        <HeaderLogo
+          src="https://res.cloudinary.com/djoprd9i4/image/upload/v1680452464/samples/picture_market/Group_128_4_zvenh9.png"
+          width="100"
+          height="75"
+          alt="logo"
+        />
+        <HeaderBtn onClick={handleClick}>
+          <HeaderBtnLin />
+          <HeaderBtnLin />
         </HeaderBtn>
-          <ListLayout>
-            <li><LinkLayout href="">About Us</LinkLayout></li>
-            <li><LinkLayout href="">Gallery</LinkLayout></li>
-            <li><LinkLayout href="">Order</LinkLayout></li>
-          </ListLayout>
-          <LinkBorder/>
+        <ListLayout>
+          <li>
+            <LinkLayout href="">About Us</LinkLayout>
+          </li>
+          <li>
+            <LinkLayout href="">Gallery</LinkLayout>
+          </li>
+          <li>
+            <LinkLayout href="">Order</LinkLayout>
+          </li>
+        </ListLayout>
+        <LinkBorder />
       </Section>
     </HeaderConteiner>
   );
-};
+}
