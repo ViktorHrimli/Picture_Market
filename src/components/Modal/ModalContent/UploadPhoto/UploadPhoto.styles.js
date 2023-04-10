4eab30d8190783784c5e210b7a0d1ccf3696de3f
Savchenko-Andrey <dracxm95@gmail.com>
2023-04-10 17:20:48 +0300

    modal and)

--- src/components/Modal/ModalContent/UploadPhoto/UploadPhoto.styles.js
+++ src/components/Modal/ModalContent/UploadPhoto/UploadPhoto.styles.js
@@ -3,36 +3,87 @@ import { GlobalPostition } from 'styles/GlobalStyles.styled';
 import { GoX } from 'react-icons/go';
 
 export const Section = styled(GlobalPostition)`
-  width: 583px;
-  padding-top: 42px;
-  padding-right: 54px;
-  padding-left: 54px;
-  padding-bottom: 72px;
+  position: relative;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    width: 312px;
+    height: 498px;
+    padding-top: 82px;
+    padding-right: 23px;
+    padding-left: 23px;
+    padding-bottom: 40px;
+  }
+
+  @media (min-width: 1440px) {
+    width: 583px;
+    padding-top: 82px;
+    padding-right: 54px;
+    padding-left: 54px;
+    padding-bottom: 72px;
+  }
 `;
 
 export const Text = styled.p`
   font-style: normal;
   font-weight: 400;
-  font-size: 28px;
-  line-height: 34px;
-  text-align: center;
   color: #003031;
+  text-align: center;
   margin-bottom: 40px;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    font-size: ${p => p.theme.fontSizes.s};
+    line-height: 21px;
+  }
+
+  @media (min-width: 1440px) {
+    font-size: ${p => p.theme.fontSizes.l};
+    line-height: 34px;
+  }
 `;
 
 export const ChooseImg = styled.div`
-  width: 350px;
-  height: 250px;
-  border: 1px solid red;
-  margin-bottom: 150px;
+  @media (min-width: 320px) and (max-width: 480px) {
+    width: 252px;
+    height: 184px;
+    border: 1px solid red;
+    margin-bottom: 78px;
+  }
+  @media (min-width: 1440px) {
+    width: 350px;
+    height: 250px;
+    margin-bottom: 150px;
+    border: 1px solid red;
+  }
 `;
 
 export const ModalClosed = styled.button`
   margin-left: auto;
   cursor: pointer;
+  position: absolute;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    top: 18px;
+    right: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20px;
+    height: 20px;
+  }
+  @media (min-width: 1440px) {
+    top: 42px;
+    right: 54px;
+  }
 `;
 
 export const Close = styled(GoX)`
   fill: ${p => p.theme.colors.main};
-  font-size: ${p => p.theme.fontSizes.xl};
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    font-size: ${p => p.theme.fontSizes.l};
+  }
+
+  @media (min-width: 1440px) {
+    font-size: ${p => p.theme.fontSizes.xl};
+  }
 `;
