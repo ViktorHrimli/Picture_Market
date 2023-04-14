23c715a53d91ad5d5b4a7f7e3cefaf2496c2983c
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-04-14 23:08:42 +0300

    desktop_gallery

--- src/components/Gallery/GalleryDesktop/GalleryDesktop.jsx
+++ src/components/Gallery/GalleryDesktop/GalleryDesktop.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import {
   ConteinerImgCenter,
   ConteinerImgRight,
@@ -6,24 +8,61 @@ import {
   GalleryTitleDesktop,
 } from 'components/Gallery/GalleryDesktop/GalleryDesktop.styled';
 
+const images = [
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680727969/mobile_delivery_1_u300vh.png',
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680881828/andrysha_special_jvnphu.jpg',
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680381248/samples/picture_market/yz2hyoladx5bbdciqbqr.jpg',
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680727969/mobile_delivery_1_u300vh.png',
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680881828/andrysha_special_jvnphu.jpg',
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680381248/samples/picture_market/yz2hyoladx5bbdciqbqr.jpg',
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680727969/mobile_delivery_1_u300vh.png',
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680881828/andrysha_special_jvnphu.jpg',
+  'https://res.cloudinary.com/djoprd9i4/image/upload/v1680381248/samples/picture_market/yz2hyoladx5bbdciqbqr.jpg',
+];
+
 const GalleryDesktop = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const timeOut = () => {
+      setInterval(() => {
+        setCount(prev => prev + 1);
+      }, 4000);
+    };
+    timeOut();
+  });
+
+  if (count >= images.length) {
+    setCount(0);
+  }
+
   return (
-    <Conteiner>
+    <>
       <GalleryTitleDesktop>
         Enjoy our <br /> <span>Gallerry</span>
       </GalleryTitleDesktop>
-      <ConteinerImgCenter>
-        <img />
-      </ConteinerImgCenter>
+      {images.map((item, id, arr) => {
+        if (id === count && count + 2 === id + 2) {
+          return (
+            <Conteiner key={item}>
+              <ConteinerImgCenter key={1}>
+                <img src={arr[id]} alt="keks" />
+              </ConteinerImgCenter>
 
-      <ConteinerImgRight>
-        <img />
-      </ConteinerImgRight>
+              <ConteinerImgRight key={2}>
+                <img src={arr[id + 1]} alt="keks" />
+              </ConteinerImgRight>
 
-      <ConteinerImgLeft>
-        <img />
-      </ConteinerImgLeft>
-    </Conteiner>
+              <ConteinerImgLeft key={3}>
+                <img src={arr[id + 2]} alt="keks" />
+              </ConteinerImgLeft>
+            </Conteiner>
+          );
+        } else {
+          return '';
+        }
+      })}
+    </>
   );
 };
 
