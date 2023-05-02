4f305c8e9fe25ece2620a9f099de3869a49d43ff
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-05-03 00:39:41 +0300

    make pagination style

---
+++ src/components/Gallery/GalleryDesk/GalleryDesk.styled.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const ButtonPagination = styled.button`
+  width: 10px;
+  height: 10px;
+
+  border-radius: 50%;
+
+  background-color: ${({ flag }) => {
+    return flag ? '#634789' : 'rgba(99, 71, 137, 0.25)';
+  }};
+
+  :is(:hover) {
+    background-color: #634789;
+
+    cursor: pointer;
+  }
+`;
+
+export const ConteinerContorol = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 20px;
+  grid-gap: 5px;
+`;
