1a0c0f16ea61d79e1988613fa54c13156a848aaa
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2023-08-15 00:40:11 +0300

    fix bag

--- src/components/Modal/ModalContent/AddYourReview/AddYourReview.jsx
+++ src/components/Modal/ModalContent/AddYourReview/AddYourReview.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 import {
   ModalSection,
@@ -16,69 +17,67 @@ import {
   ButtonForm,
 } from './AddYourReview.styles';
 
-// <<<<<<< HEAD
-
-
-export function AddYourReview({handleToggleForm }) {
-//   const { setStrategy, setGlobalModalState } = useContext(ModalStrategyContext);
-
-// =======
-// export function AddYourReview({ closeModal }) {
-// >>>>>>> e43587d99ee7c2c2a2f3d61aea700cd7c3cbe8fb
+export function AddYourReview({ handleToggleForm }) {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
   const handleSubmit = event => {
     event.preventDefault();
+
+    if (!name || !email || !message) {
+    } else {
+      axios
+        .post('https://postapi.onrender.com/api/sendphoto', {
+          name,
+          email,
+          message,
+        })
+        .then(res => console.log(res.data));
+    }
   };
 
   return (
     <ModalSection>
-    <ModalContent>
-    <Section>
+      <ModalContent>
+        <Section>
+          <ModalClosed onClick={handleToggleForm}>
+            <Close />
+          </ModalClosed>
 
-      {/* <ModalBack onClick={() => setStrategy(contentTypes.Discount)}>
+          <Text>Please add your review</Text>
 
-      <ModalBack>
+          <Form onSubmit={handleSubmit}>
+            <Input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              placeholder="Name"
+              required
+              onChange={e => setName(e.target.value)}
+            />
 
-        <Back />
-      </ModalBack> */}
-      <ModalClosed onClick={handleToggleForm}>
-        <Close />
-      </ModalClosed>
+            <Input
+              type="email"
+              id="email"
+              name="Email"
+              value={email}
+              placeholder="Email"
+              required
+              onChange={e => setEmail(e.target.value)}
+            />
+            <Message
+              onChange={e => setMessage(e.target.value)}
+              value={message}
+              name="textarea"
+              id="textarea"
+              required
+              placeholder="Message"
+            ></Message>
 
-      <Text>Please add your review</Text>
-
-      <Form onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          placeholder="Name"
-          onChange={e => setName(e.target.value)}
-        />
-
-        <Input
-          type="email"
-          id="email"
-          name="Email"
-          value={email}
-          placeholder="Email"
-          onChange={e => setEmail(e.target.value)}
-        />
-        <Message
-          onChange={e => setMessage(e.target.value)}
-          value={message}
-          name="textarea"
-          id="textarea"
-          placeholder="Message"
-        ></Message>
-        {/* <IconPhoto /> */}
-
-        <ButtonForm type="submit">send</ButtonForm>
-      </Form>
+            <ButtonForm type="submit">send</ButtonForm>
+          </Form>
         </Section>
       </ModalContent>
     </ModalSection>
--- src/components/Modal/ModalContent/Discount/Discount.jsx
+++ src/components/Modal/ModalContent/Discount/Discount.jsx
@@ -1,50 +1,71 @@
 import { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 // import { contentTypes } from '../../modal.constants';
 // import { ModalStrategyContext } from '../../ModalStrategyContext';
 
 import { AddYourReview } from '../AddYourReview/AddYourReview';
 
-import { Section,  ModalSection, ModalContent, ModalClosed, Close, TextBox, Text, TextUp, BoxBtn, Btn, Email } from './Discount.styles';
+import {
+  Section,
+  ModalSection,
+  ModalContent,
+  ModalClosed,
+  Close,
+  TextBox,
+  Text,
+  TextUp,
+  BoxBtn,
+  Btn,
+  Email,
+} from './Discount.styles';
 
+export function Discount({ handleToggleDiscont }) {
+  // const { setStrategy } = useContext(ModalStrategyContext);
 
-export function Discount({handleToggleDiscont}) {
-    // const { setStrategy } = useContext(ModalStrategyContext);
+  const [isOpenedForm, setIsOpenedForm] = useState(false);
+  const navigate = useNavigate();
 
-    const [isOpenedForm, setIsOpenedForm] = useState(false);
-
-
-    function handleToggleForm () {
+  function handleToggleForm() {
     setIsOpenedForm(!isOpenedForm);
-    }
-  
-    return (
-        <>
-        <ModalSection>
+  }
+
+  const handleClickOnHome = () => {
+    navigate('/');
+  };
+
+  return (
+    <>
+      <ModalSection>
         <ModalContent>
-        <Section>
+          <Section>
             {/* <ModalBack onClick={() => setStrategy(contentTypes.FillTheForm)}>
                 <Back />
             </ModalBack> */}
-            <ModalClosed  onClick={handleToggleDiscont}>
-                <Close />
+            <ModalClosed onClick={handleClickOnHome}>
+              <Close />
             </ModalClosed>
             <TextBox>
-                <Text>Thank you for your order!</Text>
-                        <TextUp>Our designer will contact you shortly to discuss and approve the layout for your unique string art portrait. If you have any questions, feel free to reach out to us at
-                                <Email onClick={handleToggleForm}> designer@string-world.com.</Email>
-                                We can't wait to create a special piece of art for you!
-                    </TextUp>
-                    <TextUp>Best regards</TextUp>
-                        </TextBox>
-                    <BoxBtn>
-                        <Btn onClick={handleToggleForm}>contact us</Btn>
-                        <Btn onClick={handleToggleDiscont}>home page</Btn>
-                    </BoxBtn>
-                </Section>
-            </ModalContent>
-            </ModalSection>
-            {isOpenedForm && <AddYourReview handleToggleForm={handleToggleForm} /> }
-            </>
-    )
+              <Text>Thank you for your order!</Text>
+              <TextUp>
+                Our designer will contact you shortly to discuss and approve the
+                layout for your unique string art portrait. If you have any
+                questions, feel free to reach out to us at
+                <Email onClick={handleToggleForm}>
+                  {' '}
+                  designer@string-world.com.
+                </Email>
+                We can't wait to create a special piece of art for you!
+              </TextUp>
+              <TextUp>Best regards</TextUp>
+            </TextBox>
+            <BoxBtn>
+              <Btn onClick={handleToggleForm}>contact us</Btn>
+              <Btn onClick={handleClickOnHome}>home page</Btn>
+            </BoxBtn>
+          </Section>
+        </ModalContent>
+      </ModalSection>
+      {isOpenedForm && <AddYourReview handleToggleForm={handleToggleForm} />}
+    </>
+  );
 }
